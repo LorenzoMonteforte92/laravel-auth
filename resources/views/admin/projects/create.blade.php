e1a446146e1d40250512fcc766db2cdc812e0bf3
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-06-04 12:51:26 +0200

    feat: add public img storage and created migration for image column in db

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -17,14 +17,18 @@
       @csrf
 
       <div class="mb-3">
-        <label for="name" class="form-label">Project Name</label>
-        <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}">
+          <label for="name" class="form-label">Project Name</label>
+          <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}">
       </div>
       <div class="mb-3">
           <label for="cient_name" class="form-label">Client Name</label>
           <input type="text" class="form-control" id="client_name" name="client_name" value="{{ old('client_name') }}">
-        </div>
-        <div class="mb-3">
+      </div>
+      <div class="mb-3">
+        <label for="formFile" class="form-label">Upload Image</label>
+        <input class="form-control" type="file" id="formFile">
+      </div>
+      <div class="mb-3">
           <label for="summary" class="form-label">Summmary</label>
           <textarea class="form-control" id="description" rows="15" name="summary">{{ old('summary') }}</textarea>
       </div>
